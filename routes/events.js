7c57309d7b953fcb635fff04121f1b75755a82d4
Ahmed Nader <ahmednader5012@gmail.com>
2025-03-27 17:16:38 +0200

    test final

--- routes/events.js
+++ routes/events.js
@@ -92,6 +92,7 @@ router.post('/add-event', async (req, res) => {
         numberOfSeats
     } = req.body;
 
+    try {
         // Create a new event
         const newEvent = new Event({
             date,
@@ -130,6 +131,10 @@ router.post('/add-event', async (req, res) => {
 
         req.flash('success', 'تم اضافة ميعاد جديد بنجاح');
         res.redirect('/events/calendars/' + calendar);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error creating event or seats.');
+    }  
 });
 
 // Edit Event Route
